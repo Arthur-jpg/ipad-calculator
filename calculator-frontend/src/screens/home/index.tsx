122e8730ae30471e798ade47ca918714517e3f38
Arthur <arthurschiller10@gmail.com>
2024-10-12 19:26:01 -0300

    front-end

--- calculator-frontend/src/screens/home/index.tsx
+++ calculator-frontend/src/screens/home/index.tsx
@@ -1,9 +1,26 @@
 import { useEffect, useRef, useState } from "react";
 
+import {SWATCHES} from '../../../constants'
+import { ColorSwatch, Group } from "@mantine/core";
+import { Button } from "../../components/ui/button";
+import axios from 'axios';
+
 /*
 O useRef é um hook do React que cria uma "referência" para armazenar um valor que não requer uma nova renderização do componente quando é atualizado.
 Diferente de um useState, que causa uma re-renderização quando o estado muda, o valor de um useRef pode ser modificado sem que o componente seja re-renderizado.
 */
+
+// interface para response do backend
+interface Response {
+    expr: string;
+    result: string;
+    assign: boolean
+}
+
+interface GeneratedResult {
+    expression: string;
+    answer: string;
+}
 
 export default function Home() {
     const canvasRef = useRef<HTMLCanvasElement>(null);
@@ -11,6 +28,20 @@ export default function Home() {
     O valor inicial passado para o useRef é null. Isso significa que, no momento em que o componente é montado, a referência ainda não aponta para nenhum elemento. Posteriormente, ela será "conectada" ao <canvas> real que será renderizado no DOM.
     */
     const [isDrawing, setIsDrawing] = useState(false);
+
+    const [color, setColor] = useState('rgb(255, 255, 255)')
+    const [reset, setReset] = useState(false);
+    const [result, setResult] = useState<GeneratedResult>()
+    const [dictOfVars, setDictOfVars] = useState({})
+
+    useEffect(() => {
+        if (reset) {
+            resetCanvas();
+            setReset(false);
+        }
+    }, [reset])
+    // A array tem reset para ser rodado toda vez que o reset state mudar
+
 
     useEffect(() => {
         const canvas = canvasRef.current;
@@ -28,6 +59,33 @@ export default function Home() {
         }
     }, []);
     // a array vazia faz com que este efeito só rode depois da primeira renderização
+
+    const sendData = async () => {
+        const canvas = canvasRef.current
+        if (canvas) {
+            const response = await axios({
+                method: 'post',
+                url: `${import.meta.env.VITE_API_URL}/calculate`,
+                data: {
+                    image: canvas.toDataURL('image/png'),
+                    dict_of_vars: dictOfVars,
+                }
+            })
+            const resp = await response.data;
+            console.log('Response: ', resp)
+        }
+    }
+
+    const resetCanvas = () => {
+        const canvas = canvasRef.current;
+        if (canvas) {
+            const ctx = canvas. getContext('2d');
+            if (ctx) {
+                ctx.clearRect(0, 0, canvas.width, canvas.height)
+            }
+
+        }
+    }
 
     const startDrawing = (e: React.MouseEvent<HTMLCanvasElement>) => {
         const canvas = canvasRef.current;
@@ -57,7 +115,7 @@ export default function Home() {
         if (canvas) {
             const ctx = canvas.getContext('2d');
             if (ctx) {
-                ctx.strokeStyle = 'white'
+                ctx.strokeStyle = color
                 ctx.lineTo(e.nativeEvent.offsetX, e.nativeEvent.offsetY)
                 ctx.stroke()
             }
@@ -65,6 +123,35 @@ export default function Home() {
     }
 
     return (
+    <>
+        <div className="grid grid-cols-3 gap-2">
+            <Button
+                onClick={() => setReset(true)}
+                className="z-20 bg-black text-white"
+                variant='default'
+                color="black"
+            >
+                Reset
+            </Button>
+            <Group className="z-20">
+                    {SWATCHES.map((swatchColor: string) => (
+                        <ColorSwatch
+                            key={swatchColor}
+                            color={swatchColor}
+                            onClick={() => setColor(swatchColor)}
+                            />
+                    ))}
+            </Group>
+            <Button
+                onClick={sendData}
+                className="z-20 bg-black text-white"
+                variant='default'
+                color="black"
+            >
+                Calcular
+            </Button>
+
+        </div>
         <canvas 
             ref={canvasRef}
             id='canvas'
@@ -73,6 +160,7 @@ export default function Home() {
             onMouseOut={stopDrawing}
             onMouseUp={stopDrawing}
             onMouseMove={draw}
-        />
+            />
+    </>
     )
 }
